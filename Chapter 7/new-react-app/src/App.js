c2e7c7d41d61989ef22aa551e544bbb98689126e
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-08 22:03:22 +0200

    First Commit

---
+++ Chapter 7/new-react-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import Products from './Products';
+// import JumboTronComponent from './JumboTronComponent';
+// import UserForm from './UserForm';
+import GitHub from './GitHub';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        {/* <JumboTronComponent sentence='This is a long sentence, and I want to insert content into the
+          jumbotron component from the outside.'>
+          
+        </JumboTronComponent> */}
+        {/* <Products/> */}
+        {/* <UserForm/> */}
+        <GitHub/>
+      </div>
+    );
+  }
+}
+
+export default App;
