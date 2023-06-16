ef61bc36e6f708cf08c8fb3a8a7640a39f2d5c1d
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-16 18:31:29 +0200

    Complete Project

---
+++ Projects/dynamic_table_app/src/dynamic-table/TableData.js
@@ -0,0 +1,9 @@
+const TableData = [
+    {id:1, fullName:"Rapsan Jani", age:26, city:"Noida"},
+    {id:2, fullName: 'Noor Khan', age: 25, city: 'Patna'},
+    {id:3, fullName:"Monika Singh", age:18, city:"New Delhi"},
+    {id:4, fullName:"Kajol Kumari", age: 21, city: "Chennai"},
+    {id:5, fullName:"Sunil Kumar", age:22, city: "Jaipur"}
+]
+
+export default TableData;
